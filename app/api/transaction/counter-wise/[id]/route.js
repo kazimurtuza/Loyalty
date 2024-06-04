a8f7b5ff7dda5e958d328fdb7e33a511bdd40294
unknown <sajeebchakraborty.cse2000@gmail.com>
2024-06-04 15:20:21 +0600

    sss

--- app/api/transaction/counter-wise/[id]/route.js
+++ app/api/transaction/counter-wise/[id]/route.js
@@ -17,8 +17,24 @@ export async function GET(request,content) {
     try{
         await mongoose.connect(connectionStr);
         const counterId=content.params.id;
+        
+        let query={counter: counterId};
+        const info = new URL(request.url);
+        const searchParams = info.searchParams;
+        let date = searchParams.get('date');
+        
+        if (date) {
+            const dateObj = new Date(date);
+            const nextDay = new Date(dateObj);
+            nextDay.setDate(dateObj.getDate() + 1);
+
+            query.payment_date = {
+                $gte: dateObj,
+                $lt: nextDay
+            };
+        }
        
-        orderInfo =await Order.find({counter: counterId}).populate({
+        orderInfo =await Order.find(query).populate({
             path:'counter',
             model:'counters'
         }).sort({created_at:-1});   
--- app/api/transaction/user-wise/[id]/route.js
+++ app/api/transaction/user-wise/[id]/route.js
@@ -1,35 +1,40 @@
 import { NextResponse } from "next/server";
 import mongoose from "mongoose";
 import { connectionStr } from "@/lib/db";
-import { EmployeeAssignCounter } from "@/lib/model/employeeAssignCounter";
-import { Counter } from "@/lib/model/counter";
-import { User } from "@/lib/model/users";
 import { Order } from "@/lib/model/order";
-import { PaymentSetting } from "@/lib/model/payementSetting";
-import { EarnPoint } from "@/lib/model/earnPoint";
 
-
-export async function GET(request,content) {
-
-    let orderInfo=[];
-    let earnPointInfo=[];
-    let msg="";
-    try{
-        await mongoose.connect(connectionStr);
-        const userId=content.params.id;
-       
-        orderInfo =await Order.find({user: userId}).populate({
-            path:'counter',
-            model:'counters'
-        }).sort({created_at:-1});   
-        return NextResponse.json({orderInfo,msg,success:true});
-
-        
-    }
-    catch(error)
-    {
-        msg=error.message;
-        return NextResponse.json({msg,success:false});
-    }
+export async function GET(request, content) {
+    let orderInfo = [];
+    let msg = "";
     
+    try {
+        await mongoose.connect(connectionStr);
+        const userId = content.params.id;
+        let query = { user: userId };
+        const info = new URL(request.url);
+        const searchParams = info.searchParams;
+        let date = searchParams.get('date');
+        
+        if (date) {
+            const dateObj = new Date(date);
+            const nextDay = new Date(dateObj);
+            nextDay.setDate(dateObj.getDate() + 1);
+
+            query.payment_date = {
+                $gte: dateObj,
+                $lt: nextDay
+            };
+        }
+        
+        orderInfo = await Order.find(query).populate({
+            path: 'counter',
+            model: 'counters'
+        }).sort({ created_at: -1 });
+        
+        return NextResponse.json({ orderInfo, msg, success: true, date });
+        
+    } catch (error) {
+        msg = error.message;
+        return NextResponse.json({ msg, success: false });
+    }
 }
